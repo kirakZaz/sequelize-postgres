00dfb146ac6df1ce36b2ae2207d8736c907c115c
kirakZaz <kirka.zaz@gmail.com>
2021-11-17 15:39:04 +0200

    yaroslav connection

--- server/models/mogoose/index.js
+++ server/models/mogoose/index.js
@@ -1,5 +1,6 @@
 const dbConfig = {
     url: 'mongodb+srv://kiraZa:nvzhq0gy@cluster0.ejznt.mongodb.net/robo?retryWrites=true&w=majority'
+    // url: 'mongodb+srv://test:test@yaroslav@smoothr.de/testDb?retryWrites=true&w=majority' // todo cant connect O_o
 };
 
 const mongoose = require("mongoose");
