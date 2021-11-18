c5ac7b303d20cf9a92e4eceaf4da82a700dbd2bd
kirakZaz <kirka.zaz@gmail.com>
2021-11-18 19:41:18 +0200

    swagger

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ import http from 'http';
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
 
 import router  from './server/routes';
 import db from './server/models';
+
+import swaggerDocument from './server/swagger.json';
 
 require('dotenv').config();
 
@@ -17,6 +20,10 @@ function main () {
     app.use(logger('dev'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: false}));
+    app.use('/swagger',
+        swaggerUi.serve,
+        swaggerUi.setup(swaggerDocument, { explorer: true })
+    );
 
     app.set('view engine', 'html');
 
